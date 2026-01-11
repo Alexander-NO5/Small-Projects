dbfda2d34131c2c993b5446cd22b976b77d67c0e
Alexandru Oprisan <alexandru.oprisan08@gmail.com>
2026-01-11 05:27:58 +0200

    Implement number guessing game with limited tries

---
+++ loops/guess.py
@@ -0,0 +1,11 @@
+tries = 0
+guess = 0
+
+while guess != 6 and tries < 3:
+  guess = int(input("Guess the number:  "))
+  tries = tries + 1
+  
+if guess == 6:
+  print("You got it!")
+else:
+  print("You are out of tries!")
