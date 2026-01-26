1a39b94ba708c99594b9a83063a2ffacd37bf3c1
Alexandru Oprisan <alexandru.oprisan08@gmail.com>
2026-01-26 03:12:48 +0200

    Create 🧮 Calculator.py

---
+++ functions/🧮 Calculator.py
@@ -0,0 +1,25 @@
+def add(a, b):
+  result = a + b
+  return result
+
+def substract(a, b):
+  result = a - b
+  return result
+
+def multiply(a, b):
+  result = a * b
+  return result
+
+def divide(a, b):
+  result = a / b
+  return result
+
+def exp(a, b):
+  result = a ** b
+  return result
+
+print(add(4, 2))
+print(substract(4, 2))
+print(multiply(4, 2))
+print(divide(4, 2))
+print(exp(4, 2))
