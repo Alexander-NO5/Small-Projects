cf0df9a17c76845cfe870ddec259da128527dd06
Alexandru Oprisan <alexandru.oprisan08@gmail.com>
2026-01-28 06:15:10 +0200

    Update 🎰 Slot Machine.py

--- classes_and_objects/🎰 Slot Machine.py
+++ classes_and_objects/🎰 Slot Machine.py
@@ -1,12 +1,19 @@
 import random
 
-symbols = ['🍒',' 🍇', '🍉', '7️⃣']
+def play():
+  answer = 'Y'
+  while answer == 'Y': 
+    symbols = ['🍒',' 🍇', '🍉', '7️⃣']
 
-results = random.choices(symbols, k=3)
+    results = random.choices(symbols, k=3)
 
-print(f'{results[0]} | {results[1]} | {results[2]}')
+    print(f'{results[0]} | {results[1]} | {results[2]}')
 
-if results[0] == '7️⃣' and results[1] == '7️⃣' and results[2] == '7️⃣':
-  print('Jackpot! 💰')
-else:
-  print('Thanks for playing!')
+    if results[0] == '7️⃣' and results[1] == '7️⃣' and results[2] == '7️⃣':
+      print('Jackpot! 💰')
+    else:
+      print('Thanks for playing!')
+
+    answer = input('Do you want to play again? (Y/N) ')
+
+play()
