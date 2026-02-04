5b29933f9ed0948aedd942ce8da386c3e61cc121
Alexandru Oprisan <alexandru.oprisan08@gmail.com>
2026-02-04 04:27:24 +0200

    Create 🦄 Tuples.py

---
+++ data_structures/🦄 Tuples.py
@@ -0,0 +1,16 @@
+ploiesti = (44.940399, 26.023821)
+
+bucuresti = (44.940399, 26.023821)
+
+constanta = (44.177269, 28.652880)
+
+doncaster = (53.522820, -1.128462)
+
+combined = ((44.940399, 26.023821), (44.940399, 26.023821), (44.177269, 28.652880), (53.522820, -1.128462))
+
+print(ploiesti)
+print(bucuresti)
+print(constanta)
+print(doncaster)
+
+print(combined)
