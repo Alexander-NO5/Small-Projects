4bb256dd4c9d652bb623a65d7182c19428aeb5da
Alexandru Oprisan <alexandru.oprisan08@gmail.com>
2026-02-06 05:24:13 +0200

    Add file writing functionality to Dear Diary

---
+++ file_IO/📖 Dear Diary.py
@@ -0,0 +1,4 @@
+file = open('coding_diary.txt', 'w')
+file.write('Hello, World!\n'
+           'Today I learned how to write to a file in Python.\n'
+           'It is so nice to learn new things!')
